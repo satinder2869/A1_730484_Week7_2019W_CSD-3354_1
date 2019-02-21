bdfb67aad7195bcf513273ad44cadacd4ce53589
visualstudio <visualstudio@230-PC19>
2019-02-21 15:07:26 -0500

    start

---
+++ A1_730484_Week7_2019W_CSD-3354_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A1_730484_Week7_2019W_CSD_3354_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
